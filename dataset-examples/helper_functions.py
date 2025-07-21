9b1b519345be781646a2777bd5a7d15614591711
Sharath Koovill <sharath.koovill@gmail.com>
2025-07-21 15:33:22 +0200

    fix pre-commit

--- dataset-examples/helper_functions.py
+++ dataset-examples/helper_functions.py
@@ -1,37 +1,24 @@
 from shapely import wkt
-from shapely.geometry import Polygon, LineString, mapping
+from shapely.geometry import LineString, mapping
+
 
 def convert_geometry_field(data):
     for item in data:
         try:
             geom = wkt.loads(item["geometry"])
             if geom.geom_type == "Point":
-                item["geometry"] = {
-                    "type": "Point",
-                    "coordinates": [geom.x, geom.y]
-                }
+                item["geometry"] = {"type": "Point", "coordinates": [geom.x, geom.y]}
             elif geom.geom_type == "LineString":
-                item["geometry"] = {
-                    "type": "LineString",
-                    "coordinates": [list(coord) for coord in geom.coords]
-                }
+                item["geometry"] = {"type": "LineString", "coordinates": [list(coord) for coord in geom.coords]}
             elif geom.geom_type == "MultiLineString":
                 item["geometry"] = {
                     "type": "MultiLineString",
-                    "coordinates": [
-                        [list(coord) for coord in line.coords]
-                        for line in geom.geoms
-                    ]
+                    "coordinates": [[list(coord) for coord in line.coords] for line in geom.geoms],
                 }
             elif geom.geom_type == "Polygon":
-                item["geometry"] = {
-                    "type": "Polygon",
-                    "coordinates": [
-                        [list(coord) for coord in geom.exterior.coords]
-                    ]
-                }
+                item["geometry"] = {"type": "Polygon", "coordinates": [[list(coord) for coord in geom.exterior.coords]]}
         except Exception:
-            pass  
+            pass
 
     return data
 
@@ -42,21 +29,13 @@ def wkt_to_edges(geospatial_query):
     """
     # Convert WKT string back to Polygon
     polygon = wkt.loads(geospatial_query)
-    
+
     coords = list(polygon.exterior.coords)
-    
-    lines = [
-        LineString([coords[i], coords[i + 1]])
-        for i in range(len(coords) - 1)
-    ]
+
+    lines = [LineString([coords[i], coords[i + 1]]) for i in range(len(coords) - 1)]
 
     bbox_list = [
-        {
-            "type": "Feature",
-            "geometry": mapping(line),
-            "properties": {"edge": i}
-        }
-        for i, line in enumerate(lines)
+        {"type": "Feature", "geometry": mapping(line), "properties": {"edge": i}} for i, line in enumerate(lines)
     ]
 
     return bbox_list
